3094118be3fa5a57f1acd95213be8d9098e51097
Papa-het <yerkinmm@gmail.com>
2020-10-26 16:11:06 +0600

    feat: bcrypt

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,8 @@ import express, { NextFunction, Request, Response } from "express";
 import { IControllerBase } from "../interfaces";
 import { User } from "../models/User.model";
 
+import { check, validationResult } from "express-validator";
+
 class UserController implements IControllerBase {
   public path: string = "/user";
   public router = express.Router();
@@ -14,8 +16,24 @@ class UserController implements IControllerBase {
     this.router.post(`${this.path}/register`, this.postRegister);
   }
 
-  private postRegister = (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body);
+  private postRegister = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    await check("email", "Email is not valid").isEmail().run(req);
+    await check("password", "Password must be at least 6 characters long")
+      .isLength({ min: 6 })
+      .run(req);
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        success: false,
+        errors,
+      });
+    }
+
     const user = new User({
       email: req.body.email,
       password: req.body.password,
--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt-nodejs";
 
 export type UserDocument = mongoose.Document & {
   email: string;
@@ -20,5 +21,22 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function save(next) {
+  const user = this as UserDocument;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, undefined, (err: mongoose.Error, hash) => {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
 
 export const User = mongoose.model<UserDocument>("User", userSchema);
